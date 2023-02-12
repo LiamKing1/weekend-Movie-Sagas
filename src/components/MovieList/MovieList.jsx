3da6c78d7f5d05d79d3c352f6ad988225866f520
Liam King <techy.willyk3@gmail.com>
2023-02-12 09:11:16 -0600

    Ive gotten my movie onClick button to redirect to '/details/ corresponding number' of each movie cover

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
 function MovieList() {
 
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -11,15 +13,20 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    // When a movie poster is clicked, a user should be brought to the `/details` view for that movie.
+    const onPosterClick = (movie) => {
+        history.push(`/details/${movie.id}`);
+    };
+
     return (
         <main>
             <h1>MovieList</h1>
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                        <div key={movie.id} onClick={() => onPosterClick(movie)}>
+                            <h3> {movie.title} </h3>
+                            <img src={movie.poster} alt={movie.title} />
                         </div>
                     );
                 })}
